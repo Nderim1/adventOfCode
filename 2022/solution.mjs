675cfaf60a98ad54e67b10ef4d9f22fcb6c2b1cb
Nderim <nderim.topalli@plana.earth>
2022-12-05 11:00:59 +0100

    Clean code

---
+++ 2022/solution.mjs
@@ -0,0 +1,30 @@
+import { data } from './input.mjs'
+
+let sum = 0
+let greatest = 0
+
+let splitDataPerReindeer = data.split(`
+`)
+
+let summedCaloriesPerReindeer = splitDataPerReindeer.map(d => {
+  let a = JSON.parse(d || 0)
+  if (a) {
+    sum += a
+    if (greatest < sum) {
+      greatest = sum
+    }
+
+    if (sum) {
+      return sum
+    }
+
+  } else {
+    sum = 0
+  }
+}).filter(a => !!a)
+
+console.log('First part: ', greatest)
+let sortedCaloriesDsc = summedCaloriesPerReindeer.sort((a, b) => b - a)
+
+let sumOfFirstThree = sortedCaloriesDsc.splice(0, 3).reduce((acc, data) => acc + data)
+console.log('Second part: ', sumOfFirstThree)
